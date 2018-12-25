b85578ed2a212cdbc7ee44528b0ae8c2cb1be683
caha11 <manu.adina-zada@hotmail.com>
2018-12-26 00:43:27 +0800

    Fix syntax error for problem 15.

--- 15. 3Sum.cpp
+++ 15. 3Sum.cpp
@@ -1,26 +1,31 @@
+/** Solution wasn't that hard, I was sort of on the right track. Gave up a bit fast and looked at the answer.
+	The problem I could probably come up with. Try to think longer.
+**/
 
+class Solution {
+public:
+	vector<vector<int>> threeSum(<vector<int> &nums) {
+		vector<vector<int>> ans;
+		sort(nums.begin(), nums.end());
+		for(int i = 0; i < nums.size(); i++) {
+			if((i > 0) && (nums[i] == nums[i + 1])) continue;
+				
+			int front = i + 1;
+			int back = nums.size() - 1;
 
-
-vector<vector<int>> threeSum(<vector<int> &nums) {
-
-	vector<vector<int>> ans;
-
-	sort(nums.begin(), nums.end());
-
-	for(int i = 0; i < nums.size(); i++) {
-
-		
-		int front = i + 1;
-		int back = nums.size() - 1;
-
-		while(front < back) {
-			int sum = nums[i] + nums[start] + nums[end];
-			if(sum == 0) {
-				vector<int> triplet = vector<int>(3,0);
-				triplet[0] = nums[i];
-				triplet[1] = nums[start];
-				triplet[2] = nums[end];
-				front++;
-				back--;
-			if(sum < 0) front++;
-			if(sum > 0) back--;
+			while(front < back) {
+				int sum = nums[i] + nums[start] + nums[end];
+				if(sum == 0) {
+					ans.push_back(vector<int>{nums[i], nums[start], nums[back])};
+					while(nums[start] == nums[start + 1]) start++;
+					while(nums[end] == nums[end - 1]) end--;	
+					front++;
+					back--;
+				}
+				else if(sum < 0) front++;
+				else back ++;
+			}
+		}
+		return ans;
+	}
+}
