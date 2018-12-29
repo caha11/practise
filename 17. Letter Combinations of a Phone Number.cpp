d508615b8e61e7638b9b01c2049614fe6ea01415
Manu Adina-Zada <manu.adina-zada@hotmail.com>
2018-12-30 00:00:45 +0800

    Added my interpretation of 17.

--- 17. Letter Combinations of a Phone Number.cpp
+++ 17. Letter Combinations of a Phone Number.cpp
@@ -1,24 +1,29 @@
-/** Probably wouldn't think of a solution like this
-
+/** The solution is straight forward. Just couldn't think of it for some reason. Basically keeps adding letters to string.
 **/
 
-vector<string> letterCombinations(string digits) {
-    vector<string> result;
-    if(digits.empty()) return vector<string>();
-    static const vector<string> v = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-    result.push_back("");   // add a seed for the initial case
-    for(int i = 0 ; i < digits.size(); ++i) {
-        int num = digits[i]-'0';
-        if(num < 0 || num > 9) break;
-        const string& candidate = v[num];
-        if(candidate.empty()) continue;
-        vector<string> tmp;
-        for(int j = 0 ; j < candidate.size() ; ++j) {
-            for(int k = 0 ; k < result.size() ; ++k) {
-                tmp.push_back(result[k] + candidate[j]);
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        vector<string> result;
+        if(digits.empty()) return result;
+        vector<string> loopUpArray = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        result.push_back("");
+        
+		// Basically, adding letters to the string, that were added.
+		for(int i = 0; i < digits.length(); i++) {
+            vector<string> tmp;
+            int num = digits[i] - '0';
+            const string &candidate = loopUpArray[num];
+            
+            for(int j = 0; j < candidate.size(); j++) {
+                for(int k = 0; k < result.size(); k++) {
+                    tmp.push_back(result[k] + candidate[j]);
+                }
             }
+            result.swap(tmp);
         }
-        result.swap(tmp);
+        return result;
     }
-    return result;
-}
+};
+
+
